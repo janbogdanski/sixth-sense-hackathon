c37f9778ce9ea69ab23d0edca2572ae7c5d3501a
Maria Kocon <maria.kocon@agito.pl>
2016-09-03 16:39:38 +0300

    Initialization of variables

--- sixth_sense/sixth_sense.ino
+++ sixth_sense/sixth_sense.ino
@@ -4,3 +4,25 @@
  6th Sense
  */
 
+//input value from COM port
+byte inValue = 0;
+
+//output pin numbers
+int pin1 = 3;
+int pin2 = 5;
+int pin3 = 6;
+int pin4 = 9;
+int pin5 = 10;
+int pin6 = 11;
+
+//values set to output
+int outValue1 = 0;
+int outValue2 = 0;
+int outValue3 = 0;
+int outValue4 = 0;
+int outValue5 = 0;
+int outValue6 = 0;
+
+long delayTime = 10;
+long timeout = 10000000;
+long serialSpeed = 9600;
