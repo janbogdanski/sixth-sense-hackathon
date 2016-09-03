a6f9e817b3cc1868c30e4f12a2fff4cad0748cec
Maria Kocon <maria.kocon@agito.pl>
2016-09-03 16:51:57 +0300

    added basic functions: setup, loop, reading from serial

--- sixth_sense/sixth_sense.ino
+++ sixth_sense/sixth_sense.ino
@@ -26,3 +26,31 @@ int outValue6 = 0;
 long delayTime = 10;
 long timeout = 10000000;
 long serialSpeed = 9600;
+
+void setup() 
+{
+  Serial.begin(serialSpeed);
+}
+
+void loop()
+{
+  delay(delayTime);
+ 
+  analogWrite(pin1, 0);
+  analogWrite(pin2, 0);
+  analogWrite(pin3, 0);
+  analogWrite(pin4, 0);
+  analogWrite(pin5, 0);
+  analogWrite(pin6, 0);
+}
+
+void serialEvent() 
+{
+  while (Serial.available()) 
+  {
+    Serial.setTimeout(timeout);
+    inValue = (byte)Serial.parseInt();
+    
+  }
+}
+
